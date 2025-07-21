21456806a665d3c9b8e6c16dde76a829cd644ae1
Sagar Singh Baghel <144033506+sagarrepogit@users.noreply.github.com>
2025-07-21 18:44:42 +0530

    undertanding global in deep took a lot of time revice it once more for the better depth as it is important

---
+++ Null.js
@@ -0,0 +1,16 @@
+//null
+let a =null
+console.log(a); 
+console.log(typeof a)
+//undefined
+let b= undefined
+console.log(undefined)
+console.log(typeof b)
+
+console.log(a==b);
+console.log(a===b);
+console.log(a!==b)
+
+// defining the global function
+globalThis. myname= "sagar"
+console.log(global.myname);
