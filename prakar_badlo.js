c4f093ebd6330754ad08e2193c145b8fb81e5344
Sagar Singh Baghel <144033506+sagarrepogit@users.noreply.github.com>
2025-10-08 19:59:35 +0530

    this is 8th oct

---
+++ prakar_badlo.js
@@ -0,0 +1,25 @@
+// here we will be doing the type conversion of the value in the form of the 
+//explicit type of conversion of the value that will happen in them
+
+console.log(Number("3"))
+// here you cannot write the Number("3") as numer("3")
+//because javascript is a case sensitive language where NUmber("3")is treated as s case sensitive javscroiip
+//and the javascript is a again a case sensitive language where being a case sensitive is a big issue
+console.log(String(false))
+let n=1223.44455
+n.toFixed(2)
+console.log(n)
+// this is a method through which i can basically do the explict
+// conversion of the value where the number is basically rounded off to the closest decimila place where i amtelling it to be rounded off to 
+// this is the way through which i cna basically when i can do the type casting in a ecxplicit manner
+//n.toExponential(3);
+console.log(n)
+let m=n.toPrecision(4)
+console.log(m)
+
+console.log(Number("475"))
+console.log(parseInt("546ghkghd"))
+
+let o= "2375sjshhj0"
+t=parseInt.o
+console.log(t)
